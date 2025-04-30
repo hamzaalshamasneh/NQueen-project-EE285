e70f55dbbbcb1601d18fe0b7f7e4325c834323aa
Alina Matchette <alinamatchette@gmail.com>
2025-04-30 14:46:17 -0700

    Update testfile.m with information
    added info on how to structure a test file

--- testfile.m
+++ testfile.m
@@ -1,14 +1,18 @@
-x = ones(8);
+#example test file for queenElim, with comments about how to structure a test file
+
+fBoard = ones(8); #fake board
+fPos = [5 3]; #fame M,N next result
+fPos2 = [8 8];
 
 
-run1 = (queenElim([5,3],x));
+run1 = (queenElim(fPos,fBoard));#call the actual function with sim inputs
 
-run2 = (queenElim([8,8],run1));
-run3 = (queenElim([8,8],run2));
+run2 = (queenElim(fPos2,run1));#call the actual function with sim inputs
+run3 = (queenElim(fPos2,run2));#call the actual function with sim inputs
 
 
 
-disp(run1);
+disp(run1); #displaying all outputs from the sim runs
 disp("break");
 disp(run2);
 disp("break");
