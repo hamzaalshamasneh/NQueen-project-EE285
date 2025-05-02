7fb8c7bf045ee408067dd484d7f97503633b9c92
hamzaalshamasneh <hamzahhazem612@gmail.com>
2025-05-01 20:14:40 -0700

    Update MN_next.m

--- MN_next.m
+++ MN_next.m
@@ -11,32 +11,52 @@ function [finalBoard, queenPositions] = MN_next(startCoord, queenNum)
     queenPositions = startCoord;
     
     boardMatrix = queenElim(startCoord, boardMatrix);
+    
+    %built in function to convert the (row, col) position of the queen into a linear index (used for circular search)
     currentIndex = sub2ind(size(boardMatrix), row, col);
 
+    %loop to place the remaining queens, starting from the socnd queen
     for i = 2:queenNum
+
+        %find all squares that have a value of 1
         [validRows, validCols] = find(boardMatrix == 1);
+
+        %convert all valid positions into linear indices
         validIndices = sub2ind(size(boardMatrix), validRows, validCols);
 
+        %exit the loop if there are no valid squares left
         if isempty(validIndices)
             break;
         end
 
+        %sort the valid positions in ascending order (top left to bottom right)
         validIndices = sort(validIndices);
+
+        %try to find the next square after the current one is sorted
         nextIndex = validIndices(find(validIndices > currentIndex, 1));
 
+        %in case we’ve reached the end of the valid squares on the board, go back to the first square
         if isempty(nextIndex)
             nextIndex = validIndices(1);
         end
 
+        %convert linear position back to row col
         [nextRow, nextCol] = ind2sub(size(boardMatrix), nextIndex);
         currentCoord = [nextRow, nextCol];
 
+        %place a queen at the found valid square
         boardMatrix(nextRow, nextCol) = 1;
+
+        % add the position of this queen to the list of placed queens
         queenPositions = [queenPositions; currentCoord];
 
+        %eliminate all squares that are now threatened by the newly placed queen
         boardMatrix = queenElim(currentCoord, boardMatrix);
+
+        %update currentIndex so that the next search continues from this new position
         currentIndex = nextIndex;
     end
 
+    %after placing all queens (or exiting early), return the final board and queen positions
     finalBoard = boardMatrix;
 end
