90e725ab650d6ce11c71a96dcdc905ac4567d79c
Alina Matchette <alinamatchette@gmail.com>
2025-05-02 23:12:46 -0700

    Update testfileMNvElim.m

--- testfileMNvElim.m
+++ testfileMNvElim.m
@@ -4,15 +4,17 @@ x = ones(4);
 passcount  = 0;
 failcount = 0;
 
-for k = 1:12
+for k = 11:14
 fprintf("\n\n\nTrying %d\n\n",k);
 for i = 1:k
   for j = 1:k
 
 
-  tic
-  run = evalu( MN_next([i j],k));
-  toc
+ tic
+  runa =  MN_next([i j],k);
+ toc
+  run = evalu(runa);
+
   disp(run);
   if run = "Pass"
     passcount = passcount + 1;
@@ -28,6 +30,5 @@ disp("\ndone");
 fprintf("passed %d times, failed %d times",passcount,failcount);
 end
 #run = MN_next([1 1],numq);
-
 
 
