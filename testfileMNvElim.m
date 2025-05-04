f71f6e4f037d69357f44e7d214596a4395a55420
Alina Matchette <alinamatchette@gmail.com>
2025-05-03 21:27:25 -0700

    Update testfileMNvElim.m
    added header block and comments about code function

--- testfileMNvElim.m
+++ testfileMNvElim.m
@@ -1,10 +1,35 @@
-x = ones(4);
+#{
+Description:
+  script that runs evaulU for every single board and starting position from startPoint to specified stopPoint, in terms of dimenstions of chessboard.
+  Is used to test the output of MN_next to verify that it outputs as expected for every input
+
+
+
+Inputs:
+    requres runner to change start and stop position veriables to desired values
+Output
+    returns the number of times that output passed and failed, along with the times it takes to execute and the retult of each test
+Version:
+  1.0 Original
+  1.1 - addes stop and start positions for easier user experience
+#}
+
+## Author: Alina Matchette
+## Created: 2025-05-02
+
+startPoint = 1; #SET THESE to target start and stop dimensions
+stopPoint = 11;
 
 
 passcount  = 0;
 failcount = 0;
 
-for k = 11:14
+
+
+
+
+
+for k = startPoint:stopPoint #runs loop fdor every single board and cordinate on board of MN_next
 fprintf("\n\n\nTrying %d\n\n",k);
 for i = 1:k
   for j = 1:k
@@ -12,11 +37,13 @@ for i = 1:k
 
  tic
   runa =  MN_next([i j],k);
+ disp("latteral")
  toc
-  run = evalu(runa);
-
+ tic
+  run = evalu(runa); #checking output from MN_next in evalU
+ toc
   disp(run);
-  if run = "Pass"
+  if run == "Pass" #updating pass or fail count
     passcount = passcount + 1;
    else
     failcount = failcount + 1;
@@ -25,10 +52,9 @@ for i = 1:k
 
 end
 end
-disp("\ndone");
+disp("\ndone"); #final display of passcount and failcount
 
 fprintf("passed %d times, failed %d times",passcount,failcount);
 end
-#run = MN_next([1 1],numq);
 
 
